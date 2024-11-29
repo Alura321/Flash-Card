db7d5ab6db085c4d9dff1506661a96b6dbf778ad
Alura321 <00001105323675sp@al.educacao.sp.gov.br>
2024-11-29 01:50:39 -0300

    Add files via upload
    Teste

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    "História",
+    "Quando acabou a Segunda Guerra Mundial?",
+    "No ano de 1945"
+)
+
+criaCartao(
+    "Geografia",
+    "Qual a capital da França?",
+    "A capital da França é Paris"
+)
+
+criaCartao(
+    "Ciências",
+    "Qauntos biomas existem no Brasil?",
+    "Existem 6 biomas no Brasil "
+)
+
+criaCartao(
+    "Idioma",
+    "Como se diz amor em inglês?",
+    "Amor em inglês é LOVE"
+)
+
+criaCartao(
+    "Matemática",
+    "Qual a raiz quadrada de 4?",
+    "A raiz quadrada de 4 é 2"
+)
